d4fdf29fb035067db63703e2a0b980888cd078c9
Heikal Syah Shiddiq <heikalsyahshiddiq@gmail.com>
2023-11-30 17:38:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -152,6 +152,3 @@ async def colab(item: User_colab):
     result_dict = dict(zip(user_data.iloc[top_n_colab]['User_ID'].tolist(), similarities_preference[top_n_colab]))
 
     return item.user_id, result_dict
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
